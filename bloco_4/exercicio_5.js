dd7456a0539b6e0d8a36522b174cfd4de8d8651a
IsleneGomes <islenegomes@gmail.com>
2021-03-24 19:24:04 -0300

    Programa 5

---
+++ bloco_4/exercicio_5.js
@@ -0,0 +1,11 @@
+let anguloA = 30;
+let anguloB = 70;
+let anguloC = 80;
+
+let somaDosAngulos = anguloA + anguloB + anguloC;
+
+if (somaDosAngulos === 180) {
+    console.log("true")
+} else {
+    console.log("false")
+}
