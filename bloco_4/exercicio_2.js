3cb6546ccce54010eb483e5cb1da3e2abc7d1730
IsleneGomes <islenegomes@gmail.com>
2021-03-24 18:46:52 -0300

    Programa 2

---
+++ bloco_4/exercicio_2.js
@@ -0,0 +1,9 @@
+let numA= 85;
+let numB= 100;
+ 
+if (numA > numB){
+    console.log("O número ", numA, " é maior que o número ", numB);
+}
+else {
+    console.log("O número ", numB, " é maior que o número ", numA);
+}
