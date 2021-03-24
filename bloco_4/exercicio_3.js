080c5e074e7ecae13195290b6384ed4116fc4562
IsleneGomes <islenegomes@gmail.com>
2021-03-24 18:52:06 -0300

    Programa 3

---
+++ bloco_4/exercicio_3.js
@@ -0,0 +1,13 @@
+let a = 25;
+let b = 85;
+let c = -3;
+
+if (a > b & a > c) {
+    console.log("O número ", a, " é maior que o número ", b, " e ", c);
+}
+else if (b > a & b > c) {
+    console.log("O número ", b, " é maior que o número ", a, " e ", c);
+}
+else {
+    console.log("O número ", c, " é maior que o número ", a, " e ", b);
+}
