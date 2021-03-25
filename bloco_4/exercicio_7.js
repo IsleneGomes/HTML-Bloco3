98fa06eea93c51b37f76e32e102d0499ac9c991a
IsleneGomes <islenegomes@gmail.com>
2021-03-24 21:05:09 -0300

    Programa 7

---
+++ bloco_4/exercicio_7.js
@@ -0,0 +1,15 @@
+let porcentagem = 85;
+
+if (porcentagem >= 90) {
+    console.log("A");
+}else if (porcentagem >= 80 & porcentagem < 90) {
+    console.log("B");
+}else if (porcentagem >= 70 & porcentagem < 80) {
+    console.log("C");
+}else if (porcentagem >= 60 & porcentagem < 70) {
+    console.log("D");
+}else if (porcentagem >= 50 & porcentagem < 60) {
+    console.log("E");
+}else {
+    console.log("F");
+}
