9233636625451309fb55f5ce891c466b91e30125
IsleneGomes <islenegomes@gmail.com>
2021-04-17 18:16:21 -0300

    6.1

---
+++ Bloco_6/script.js
@@ -0,0 +1,13 @@
+const estadosUF = document.getElementById('estado');
+const uf = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
+
+
+function estadosBras() {
+  for (let index = 0; index < uf.length; index += 1) {
+    const option = document.createElement('option');
+    estadosUF.appendChild(option);
+    option.innerText = uf[index];
+  }
+}
+estadosBras();
+
