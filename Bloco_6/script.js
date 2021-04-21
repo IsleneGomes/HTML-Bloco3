2697c1c6ce51d4069e9352a992495111694f0a2a
IsleneGomes <islenegomes@gmail.com>
2021-04-21 15:35:52 -0300

    6.2

--- Bloco_6/script.js
+++ Bloco_6/script.js
@@ -1,6 +1,5 @@
-const bootstrap = require(bootstrap.js);
-const estadosUF = document.getElementsByTagName('.combobox');
-const uf = [" ", "AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
+const estadosUF = document.getElementById('estado');
+const uf = ["", "AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
 
 function estadosBras() {
   for (let index = 0; index < uf.length; index += 1) {
@@ -10,3 +9,18 @@ function estadosBras() {
   }
 }
 estadosBras();
+
+function cpf() {
+const cpf = document.getElementById('cpf');
+let valor = cpf.value;
+  if (valor !== 'text') {
+    return cpf.value;
+  } else {
+    alert ('Insira apenas números.');
+  }
+}
+cpf();
+
+function preventDefault(element) {
+  element.preventDefault();
+}
