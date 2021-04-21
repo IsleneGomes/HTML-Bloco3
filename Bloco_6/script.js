dcd7670208e06a1fffb4be7cb065f58576bca452
IsleneGomes <islenegomes@gmail.com>
2021-04-21 16:01:38 -0300

    6.2 - Lembrar de estudar mais Bibliotecas

--- Bloco_6/script.js
+++ Bloco_6/script.js
@@ -1,5 +1,5 @@
 const estadosUF = document.getElementById('estado');
-const uf = ["", "AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
+const uf = ["Selecione o Estado", "AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
 
 function estadosBras() {
   for (let index = 0; index < uf.length; index += 1) {
@@ -24,3 +24,4 @@ cpf();
 function preventDefault(element) {
   element.preventDefault();
 }
+
