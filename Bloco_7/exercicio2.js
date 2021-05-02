ad0d895a9c600f5d3e406182c4cd6f882c282074
IsleneGomes <islenegomes@gmail.com>
2021-05-02 13:39:50 -0300

    Exercicio 3 - Parte II

--- Bloco_7/exercicio2.js
+++ Bloco_7/exercicio2.js
@@ -1,4 +1,18 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-oddsAndEvens.sort((a, b) => a - b);
+function ordem() {
+const newarray = [];
+  for (let i = 0; i < oddsAndEvens.length; i += 1) {
+    for (let j = 0;  j < oddsAndEvens[i]; j += 1) {
+        newarray.push(oddsAndEvens[i]); //que doideira!
+        console.log(newarray);
+      }
+    }
+  }
+
+
+
+console.log(`Os números ${ordem()} se encontram ordenados de forma crescente!`);
+
+oddsAndEvens.sort((oddsAndEvens, b) => oddsAndEvens - b);
 
 console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
