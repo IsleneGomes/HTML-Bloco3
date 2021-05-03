59ea2d810b61f6e457355d3e29a86d50dc7f1f8c
IsleneGomes <islenegomes@gmail.com>
2021-05-03 18:45:16 -0300

    Bloco 8

--- Bloco_7/7.2/exercicio1Parte2.js
+++ Bloco_7/7.2/exercicio1Parte2.js
@@ -32,3 +32,23 @@ const arraylength = (obj) => Object.keys(obj).length;
 const value = (obj) => Object.value(obj);
 
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+
+const estudantes = () => {
+const total = allLessons.lesson1.numeroEstudantes + allLessons.lesson2.numeroEstudantes + allLessons.lesson3.numeroEstudantes;
+return total
+}
+console.log(estudantes());
+
+const position = (obj, index) => Object.value(obj, index);
+
+const existencia = (obj, key, value) => {
+const pares = Object.entries(obj);
+const isEqual = false;
+  for (let index in pares) {
+    if (pares[index][0] === key && pares[index][1] === value) {
+      isEqual === true;
+    }
+  }
+  return isEqual;
+}
+console.log(existencia(lesson2,'professor','Carlos'));
