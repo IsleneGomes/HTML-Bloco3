35f09e5bc5cede347a26f8ff4c7eae0d00de3c72
IsleneGomes <islenegomes@gmail.com>
2021-05-06 14:08:52 -0300

    Exercicio 5

--- Bloco_8/8.5/exercicio5.js
+++ Bloco_8/8.5/exercicio5.js
@@ -2,10 +2,11 @@ const assert = require('assert');
 
 const myList = [1, 2, 3];
 
-// escreva swap abaixo
+const swap = (number => ([number[2], number[1], number[0]]))
 
 const swappedList = swap(myList);
 
+console.log(swappedList[0], 3)
 assert.strictEqual(swappedList[0], 3);
 assert.strictEqual(swappedList[1], 2);
 assert.strictEqual(swappedList[2], 1);
